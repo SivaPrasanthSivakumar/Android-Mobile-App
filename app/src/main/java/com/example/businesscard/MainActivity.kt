e092f4ffbd70ef75a70633608dd56e028a49dae5
SivaPrasanthSivakumar <sivaprasanth@gmail.com>
2025-01-24 13:08:51 -0500

    update

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,9 +17,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.businesscard.ui.theme.BusinessCardTheme
-import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,9 +29,9 @@ class MainActivity : ComponentActivity() {
             BusinessCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     AndriodLogoImage(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
@@ -36,24 +39,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AndriodLogoImage(name: String, modifier: Modifier = Modifier) {
+fun AndriodLogoImage(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.android_logo)
     Box(modifier){
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.FillBounds,
-            //modifier = Modifier.fillMaxSize()
-        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+        ){
+            Image(
+                painter = image,
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth()
+            )
+            NameTitleText(
+                name = stringResource(R.string.name_text),
+                title = stringResource(R.string.title_text),
+                modifier = Modifier
+            )
+            ContactInfoText(
+                phoneNum= stringResource(R.string.phone_num_text),
+                hashhtag= stringResource(R.string.hash_tag_text),
+                email= stringResource(R.string.email_text),
+                modifier= Modifier)
+        }
+
+
     }
 
+}
+@Composable
+fun NameTitleText(name: String, title: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier ,
+        verticalArrangement = Arrangement.Center,
+    )
+    {
+        Text(
+            text = name
+        )
+        Text(
+            text = title
+        )
+    }
+}
+
+@Composable
+fun ContactInfoText(phoneNum: String, hashhtag: String, email: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize() ,
+        verticalArrangement = Arrangement.Center,
+    )
+    {
+        Text(
+            text = phoneNum
+        )
+        Text(
+            text = hashhtag
+        )
+        Text(
+            text = email
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardTheme {
-        AndriodLogoImage( name = "Android",
-            modifier = Modifier)
+        AndriodLogoImage(modifier = Modifier)
     }
 }
