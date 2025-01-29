cb6dcf39b8b8248920d65493c7b3bd685dadb54b
SivaPrasanthSivakumar <sivaprasanth@gmail.com>
2025-01-29 11:45:47 -0500

    update

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -35,21 +34,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
             BusinessCardTheme {
-                Scaffold(modifier = Modifier
-                    .fillMaxSize()
-                    ) { innerPadding ->
                     AndriodLogoImage(
                         modifier = Modifier
-                            .padding(innerPadding)
                             .background(Color(0xFF3ddc84))
                     )
 
                 }
             }
         }
-    }
 }
 
 @Composable
